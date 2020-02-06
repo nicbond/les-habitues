9802d4616f7dc3aca35d120e856618d818a45e4e
nicbond <nicolasmartins78650@free.fr>
2020-02-06 09:47:38 +0100

    Created or updated shops if already exist in database

--- src/AppBundle/Controller/ShopController.php
+++ src/AppBundle/Controller/ShopController.php
@@ -35,24 +35,40 @@ class ShopController extends FOSRestController
 		$size = $size-1;
 		
 		$em = $this->getDoctrine()->getManager();
-		
 		$i = 0;
 
 		do {
-			$shop = new Shop();
-			$shop->setNameShop($datas['data'][$i]['chain']);
-			$shop->setAdress($datas['data'][$i]['localisations'][0]['address']);
-			$shop->setZipCode($datas['data'][$i]['localisations'][0]['zipcode']);
-			$shop->setCity($datas['data'][$i]['localisations'][0]['city']);
-			$shop->setImage($datas['data'][$i]['picture_url']);
-			$shop->setOffer($datas['data'][$i]['offers'][0]['reduction']);
-			$shop->setIdShop($datas['data'][$i]['objectID']);
-			
-			$em->persist($shop);
-			$em->flush();
-			
+			$id_shop = $datas['data'][$i]['objectID'];
+			$shopSearch = $em->getRepository('AppBundle:Shop')->findOneBy(array('id_shop' => $id_shop));
+
+				try {
+						if (is_null($shopSearch)) {
+							$shop = new Shop();
+							$shop->setNameShop($datas['data'][$i]['chain']);
+							$shop->setAdress($datas['data'][$i]['localisations'][0]['address']);
+							$shop->setZipCode($datas['data'][$i]['localisations'][0]['zipcode']);
+							$shop->setCity($datas['data'][$i]['localisations'][0]['city']);
+							$shop->setImage($datas['data'][$i]['picture_url']);
+							$shop->setOffer($datas['data'][$i]['offers'][0]['reduction']);
+							$shop->setIdShop($datas['data'][$i]['objectID']);
+							$em->persist($shop);
+						} else {
+							$shopBDD = $em->getRepository('AppBundle:Shop')->find($shopSearch->getId());
+							$shopBDD->setNameShop($datas['data'][$i]['chain']);
+							$shopBDD->setAdress($datas['data'][$i]['localisations'][0]['address']);
+							$shopBDD->setZipCode($datas['data'][$i]['localisations'][0]['zipcode']);
+							$shopBDD->setCity($datas['data'][$i]['localisations'][0]['city']);
+							$shopBDD->setImage($datas['data'][$i]['picture_url']);
+							$shopBDD->setOffer($datas['data'][$i]['offers'][0]['reduction']);
+							$shopBDD->setIdShop($datas['data'][$i]['objectID']);
+							$em->merge($shopBDD);
+						}
+						$em->flush();
+					} catch (\Doctrine\ORM\ORMException $e) {
+						$errorMsg = 'Error Doctrine for the id_shop '.$datas['data'][$i]['objectID'].'<br/>'.$e->getMessage();
+					}
 			$i++;
 		} while ($i <= $size);
-		return new Response('CREATED SHOPS', Response::HTTP_CREATED);
+		return new Response('CREATED / UPDATED SHOPS', Response::HTTP_CREATED);
     }
 }

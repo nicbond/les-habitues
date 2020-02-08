d0a1a9e4ade8d668840af841b3ce7e25182db70f
nicbond <nicolasmartins78650@free.fr>
2020-02-08 18:39:59 +0100

    Switch case http_code method

--- src/AppBundle/Service/CurlHTTP.php
+++ src/AppBundle/Service/CurlHTTP.php
@@ -35,4 +35,28 @@ class CurlHTTP
 		$this->container = $container;
         $this->entityManager = $entityManager; 
     }
+	
+	public function getHttpCode($httpCode, $request)
+    {	
+		$response = null;
+		
+		switch ($httpCode) {
+			case 200:
+				$response = new Response('ACTION SUCCESS', Response::HTTP_OK);
+				break;
+			case 201:
+				$response = new Response('SHOP CREATED OR UPDATED', Response::HTTP_CREATED);
+				break;
+			case 404:
+				$response = new Response('API NOT FOUND', Response::HTTP_NOT_FOUND);
+				break;
+			case 500:
+				$response = new Response('INTERNAL SERVER ERROR', Response::HTTP_INTERNAL_SERVER_ERROR);
+				break;
+			default:
+				$response = new Response('UNDOCUMENTED ERROR', Response::HTTP_INTERNAL_SERVER_ERROR);
+				break;
+		}
+		return $response;
+	}
 }
